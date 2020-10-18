6216cb5e7cad37632e13b9a279154fff012cfc8f
Jonnas Costa <jonn567@gmail.com>
2020-10-18 12:27:00 +0100

    reactor: breaking change all config files docker etc.

---
+++ start-app-dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#if fails at any point break the script
+set -e
+ 
+#read vars from file
+env_file=".env.dev"
+source .env.dev
+export $(cut -d= -f1 $env_file)
+echo "##############################################"
+echo "#     START POSTGRES CONTAINER FIRST!        #"
+echo "##############################################"
+echo "##############################################"
+echo "  It's supposed to run with venv activated!"
+echo "##############################################"
+
+echo "Running python App ..."
+cd app && python manage.py runserver 0.0.0.0:8000
