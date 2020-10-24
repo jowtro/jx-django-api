409c3b75c2d61c375ed9833f6937c8fdd6aeb28f
Jonnas Costa <jonn567@gmail.com>
2020-10-24 16:58:05 +0100

    feature: release posts endpoint

--- app/posts/serializers.py
+++ app/posts/serializers.py
@@ -1,9 +1,24 @@
-from django.db.models import fields
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Vote
 
 
 class PostSerializer(serializers.ModelSerializer):
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
+    # virtual field
+    votes = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields = ['id','title','url','poster','created']
+        fields = ['id', 'title', 'url', 'poster',
+                  'poster_id', 'created', 'votes']
+
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
+
+
+class VoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vote
+        fields = ['id']
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,9 +1,64 @@
-from posts.serializers import PostSerializer
-from posts.models import Post
-from django.shortcuts import render
-from rest_framework import generics
+from django.http import request
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from posts.serializers import PostSerializer, VoteSerializer
+from posts.models import Post, Vote
+# from django.shortcuts import render
+from rest_framework import generics, permissions, mixins, status
 
 
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+
+
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'],
+                                   poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(r"This isn't your post to delete.")
+
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
+        serializer.save(voter=self.request.user,
+                        post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        """An user can delete his own vote
+
+        Args:
+            request (any): default args
+
+        Raises:
+            ValidationError: Something bad will happen
+
+        Returns:
+            http_status: Will return http 204
+        """
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted for this post...')
